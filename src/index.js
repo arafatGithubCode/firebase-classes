fb72a271be89d235702f2543682ade0e67ab881d
arafatGithubCode <md489288@gmail.com>
2023-12-14 20:47:09 +0600

    Setting up Firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("welcome");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4pS49b7BgVQ63yHef2WLp1Wu2qWCUy-Y",
+  authDomain: "fir-setup89.firebaseapp.com",
+  projectId: "fir-setup89",
+  storageBucket: "fir-setup89.appspot.com",
+  messagingSenderId: "141911909188",
+  appId: "1:141911909188:web:8aa26e6d8c3e949845ad11",
+};
+
+initializeApp(firebaseConfig);
